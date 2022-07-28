9a21c6652f9de888e291219b319321f665988e50
masudrana08 <rana028511@gmail.com>
2022-07-28 08:24:20 +0600

    file name organized

--- test.js
+++ test.js
@@ -9,3 +9,21 @@
 // const x = {name:'masud'}
 // x.test()
 
+
+function x(){
+    console.log('x');
+}
+function y(){
+    console.log('y');
+}
+
+function a(callback){
+    console.log('a');
+    return callback()
+}
+function b(callback){
+    console.log('b');
+     callback()
+}
+
+b(x)
